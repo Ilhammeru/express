984093a104015aeaea711453cc7e436e68b6137e
Ilhammeru <gumilang.dev@gmail.com>
2022-02-24 03:19:13 +0700

    change app.js

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/api', (req, res) => {
+    const path = `/api/item/product`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  });
+  
+  app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+  });
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
